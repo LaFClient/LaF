9af67d47eb2e0cf7755f351f589704e7775ed1da
Hiro527 <hiro527.dev@gmail.com>
2021-01-27 18:14:00 +0900

    最低限の実装

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# LaF
-Light and Fast.
+# What is LaF?
+**Laf = Light and Fast**</br>
+Krunkerはとても軽いゲームです。故に「公式クライアントでギリギリ遊べてる・・・」という方も多いはずです。そんな方でも快適に遊べるクライアントを目指して開発している順日本製Krunkerクライアントです。</br>
+
+# Attention
+**THIS SOFTWARE IS IN ALPHA VERSION. DON'T USE THIS SOFTWARE FOR EXCEPT DEVELOPPING OR TESTING.**</br>
+**このソフトウェアは現在アルファ版です。決して開発やテスト目的以外での使用はしないでください。**
+
+# Contact
+e-mail: hiro527.dev@gmail.com</br>
+Discord: Hiro527#0527</br>
+Twitter: [Hiro](https://twitter.com/zHiro527)
---
+++ app/main.js
@@ -0,0 +1,64 @@
+const { app, BrowserWindow, getCurrentWindow, clipboard } = require("electron");
+const localShortcut= require("electron-localshortcut")
+
+let gameWindow = null
+
+const initFlags = () => {
+    app.commandLine.appendSwitch("disable-frame-rate-limit"); // 将来的には設定で変更可能にする
+    app.commandLine.appendSwitch("enable-zero-copy")
+};
+
+const initGameWindow = () => {
+    gameWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            contextIsolation: false
+        }
+    });
+    gameWindow.setMenuBarVisibility(false)
+
+    gameWindow.loadURL("https://krunker.io");
+
+    initShortcutKeys();
+    gameWindow.on("closed", () => {
+        gameWindow = null;
+    });
+};
+
+const initShortcutKeys = () => {
+    const sKeys = [
+        ["Esc", () => {             // ゲーム内でのESCキーの有効化
+            gameWindow.webContents.send("esc")
+        }], 
+        ["F5", () => {              // リ↓ロ↑ードする
+            getCurrentWindow.reload()
+        }],
+        ["F6", () => {              // 別のマッチへ
+            gameWindow.loadURL("https://krunker.io")
+        }],
+        ["F7", () => {              // クリップボードへURLをコピー(実質Inviteボタン)
+            clipboard.writeText(gameWindow.webContents.getURL())
+        }],
+        ["F8", () => {              // クリップボードのURLへアクセス(実質Joinボタン)
+            let copiedText = clipboard.readText();
+            if (copiedText.substr(0, 25) === "https://krunker.io/?game=" || copiedText.substr(0, 30) === "https://comp.krunker.io/?game=") gameWindow.loadURL(copiedText);
+        }],
+        ["Ctrl+Shift+F1", () => {   // クライアントの再起動
+            app.relaunch();
+            app.quit();
+        }],
+        ["Ctrl+F1", () => {         // 開発者ツールの起動
+            gameWindow.webContents.openDevTools()
+        }]
+    ];
+
+    sKeys.forEach((k) => {
+            localShortcut.register(gameWindow, k[0], k[1])
+        });
+};
+
+app.on("ready", () => {
+    initFlags();
+    initGameWindow();
+});
