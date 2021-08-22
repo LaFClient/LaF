e5d8d4eed818057515fb8db3ac6c9b64f18795de
Hiro <hiro527.dev@gmail.com>
2021-08-22 17:30:29 +0900

    [Add] ロゴアニメーションに対応

--- app/js/preload/splash.js
+++ app/js/preload/splash.js
@@ -1,5 +1,17 @@
 const { ipcRenderer } = require('electron');
 
+const CSSs = [
+    '../css/keyframes/splash-rolling1.css',
+    '../css/keyframes/splash-drop1.css',
+];
+
+window.onload = () => {
+    const el = document.createElement('link');
+    el.setAttribute('rel', 'stylesheet');
+    el.setAttribute('href', CSSs[Math.round(Math.random() * (CSSs.length - 1))]);
+    document.getElementsByTagName('head')[0].appendChild(el);
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     ipcRenderer.invoke('getAppVersion').then((v) => {
         document.getElementById('appVersion').innerText = `LaF v${v}`;
