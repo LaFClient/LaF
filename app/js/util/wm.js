55c5778eb11093741fe663e7d3e7de13b11c72e2
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-22 13:05:12 -0500

    formatting & things
    I know I'm pushing to main this time lmao, not a breaking change this time lol

--- app/js/util/wm.js
+++ app/js/util/wm.js
@@ -1,11 +1,11 @@
 require('v8-compile-cache');
 const { app, BrowserWindow, clipboard, dialog, shell } = require('electron');
-const path = require('path');
-const fs = require('fs');
+const localShortcut = require('electron-localshortcut');
 const store = require('electron-store');
 const log = require('electron-log');
-const localShortcut = require('electron-localshortcut');
 const tools = require('./tools');
+const path = require('path');
+const fs = require('fs');
 
 const osType = process.platform;
 const config = new store();
@@ -17,49 +17,35 @@ const isSwapperEnabled = config.get('enableResourceSwapper', true);
 const ezCSSMode = config.get('easyCSSMode', 'disable');
 const isEzCSSEnabled = ezCSSMode !== 'disable';
 const langPack = require(config.get('lang', 'en_US') === 'ja_JP' ? '../../lang/ja_JP' : '../../lang/en_US');
+const windows = { game: null, hub: null, viewer: null, editor: null };
 
 log.info('Script Loaded: js/util/wm.js');
-
-const windows = {
-    game: null,
-    hub: null,
-    viewer: null,
-    editor: null,
-};
-
 const initSwapper = (win) => {
     const swapPath = path.join(app.getPath('documents'), '/LaFSwap');
+    const urls = [];
+    
     if (!fs.existsSync(swapPath)) {
         fs.mkdir(swapPath, { recursive: true }, e => {
             log.warn('ERROR IN RESOURCE SWAPPER');
             log.warn(e);
         });
-    }
-    const urls = [];
-    const recursiveFolder = (win, prefix = '') => {
+    }; const recursiveFolder = (win, prefix = '') => {
         try {
             fs.readdirSync(path.join(swapPath, prefix), { withFileTypes: true }).forEach((cPath) => {
-                if (cPath.isDirectory()) {
-                    recursiveFolder(win, `${prefix}/${cPath.name}`);
-                }
+                if (cPath.isDirectory()) recursiveFolder(win, `${prefix}/${cPath.name}`);
                 else {
                     const name = `${prefix}/${cPath.name}`;
                     const isAsset = /^\/(models|textures|sound)($|\/)/.test(name);
-                    if (isAsset) {
-                        urls.push(`*://assets.krunker.io${name}`, `*://assets.krunker.io${name}?*`);
-                    }
-                    else {
-                        urls.push(`*://krunker.io${name}`, `*://krunker.io${name}?*`, `*://comp.krunker.io${name}`, `*://comp.krunker.io${name}?*`);
-                    }
+                    
+                    if (isAsset) urls.push(`*://assets.krunker.io${name}`, `*://assets.krunker.io${name}?*`);
+                    else urls.push(`*://krunker.io${name}`, `*://krunker.io${name}?*`, `*://comp.krunker.io${name}`, `*://comp.krunker.io${name}?*`);
                 }
             });
-        }
-        catch (e) {
+        } catch (e) {
             log.warn('ERROR IN RESOURCE SWAPPER');
             log.warn(e);
         }
-    };
-    recursiveFolder(win);
+    }; recursiveFolder(win);
     if (urls.length) {
         win.webContents.session.webRequest.onBeforeRequest({ urls: urls }, (details, callback) => callback({
             redirectURL: 'laf:/' + path.join(swapPath, new URL(details.url).pathname),
@@ -83,86 +69,54 @@ exports.gameWindow = class {
                 contextIsolation: false,
                 preload: path.join(__dirname, '../preload/game.js'),
             },
-        });
-        const initShortcutKeys = () => {
+        }); const initShortcutKeys = () => {
             const sKeys = [
-                ['Esc', () => {
-                    // ゲーム内でのESCキーの有効化
-                    brWin.webContents.send('ESC');
-                }],
-                ['F4', () => {
-                    brWin.webContents.send('joinMatch');
-                }],
-                ['F5', () => {
-                    // リ↓ロ↑ードする
-                    brWin.reload();
-                }],
-                ['F6', () => {
-                    // 別のマッチへ
-                    brWin.loadURL('https://krunker.io');
-                }],
-                ['F7', () => {
-                    // クリップボードへURLをコピー(実質Inviteボタン)
-                    clipboard.writeText(brWin.webContents.getURL());
-                }],
-                ['F8', () => {
-                    // クリップボードのURLへアクセス(実質Joinボタン)
+                ['Esc', () => { brWin.webContents.send('ESC') }],                   // ゲーム内でのESCキーの有効化
+                ['F4', () => { brWin.webContents.send('joinMatch') }],              //
+                ['F5', () => { brWin.reload() }],                                   // リ↓ロ↑ードする
+                ['F6', () => { brWin.loadURL('https://krunker.io') }],              // 別のマッチへ
+                ['F7', () => { clipboard.writeText(brWin.webContents.getURL()) }],  // クリップボードへURLをコピー(実質Inviteボタン)
+                ['F8', () => {                                                      // クリップボードのURLへアクセス(実質Joinボタン)
                     const copiedText = clipboard.readText();
                     if (lafTools.urlType(copiedText) === 'game') brWin.loadURL(copiedText);
-                }],
-                ['F11', () => {
+                }], ['F11', () => {
                     const isFullScreen = brWin.isFullScreen();
                     config.set('Fullscreen', !isFullScreen);
                     brWin.setFullScreen(!isFullScreen);
-                }],
-                ['Ctrl+Shift+F1', () => {
-                    // クライアントの再起動
-                    app.relaunch();
-                    app.quit();
-                }],
-                [
-                    ['Ctrl+F1', 'F12'], () => {
-                        // 開発者ツールの起動
+                }], ['Ctrl+Shift+F1', () => {     // クライアントの再起動
+                    app.relaunch();               //
+                    app.quit();                   //
+                }], [['Ctrl+F1', 'F12'], () => {  // 開発者ツールの起動
                         brWin.webContents.openDevTools();
                     },
                 ],
-            ];
-
-            sKeys.forEach((k) => {
+            ]; sKeys.forEach((k) => {
                 localShortcut.register(brWin, k[0], k[1]);
             });
         };
 
         brWin.removeMenu();
         initShortcutKeys();
-        if (isSwapperEnabled) {
-            initSwapper(brWin);
-        }
+        if (isSwapperEnabled) initSwapper(brWin);
         brWin.loadURL('https://krunker.io');
-
-        // イベントハンドラ
-        brWin.once('ready-to-show', () => {
+        brWin.once('ready-to-show', () => { // イベントハンドラ
             if (config.get('isMaximized', false)) brWin.maximize();
             if (config.get('Fullscreen', false)) brWin.setFullScreen(true);
             brWin.show();
         });
-        brWin.webContents.on('did-finish-load', () => {
-            brWin.webContents.send('didFinishLoad');
-        });
-        brWin.on('page-title-updated', (e) => {
-            e.preventDefault();
-        });
+        
+        brWin.webContents.on('did-finish-load', () => { brWin.webContents.send('didFinishLoad') });
+        brWin.on('page-title-updated', (e) => { e.preventDefault() });
         brWin.on('close', () => {
             const isMaximized = brWin.isMaximized();
-            config.set('isMaximized', isMaximized);
             const isFullScreen = brWin.isFullScreen();
+            
+            config.set('isMaximized', isMaximized);
             config.set('Fullscreen', isFullScreen);
-        });
-        brWin.webContents.on('new-window', (e, url) => {
+        }); brWin.webContents.on('new-window', (e, url) => {
             e.preventDefault();
             openNewWindow(url);
-        });
-        return brWin;
+        }); return brWin;
     }
 };
 
@@ -180,36 +134,19 @@ exports.socialWindow = class {
         });
         const initShortcutKeys = () => {
             const sKeys = [
-                ['Esc', () => {
-                    // ゲーム内でのESCキーの有効化
-                    // eslint-disable-next-line semi
-                    brWin.webContents.send('ESC')
-                }],
-                ['F5', () => {
-                    // リ↓ロ↑ードする
-                    brWin.reload();
-                }],
-                ['F7', () => {
-                    // クリップボードへURLをコピー(実質Inviteボタン)
-                    clipboard.writeText(brWin.webContents.getURL());
-                }],
+                ['Esc', () => { brWin.webContents.send('ESC') }],                  // ゲーム内でのESCキーの有効化 (eslint-disable-next-line semi)
+                ['F5', () => { brWin.reload() }],                                  // リ↓ロ↑ードする
+                ['F7', () => { clipboard.writeText(brWin.webContents.getURL()) }], // クリップボードへURLをコピー(実質Inviteボタン)
                 ['F11', () => {
                     const isFullScreen = brWin.isFullScreen();
                     config.set('Fullscreen', !isFullScreen);
                     brWin.setFullScreen(!isFullScreen);
-                }],
-                ['F12', () => {
-                    // 開発者ツールの起動
-                    brWin.webContents.openDevTools();
-                }],
-                ['Ctrl+Shift+F1', () => {
-                    // クライアントの再起動
+                }], ['F12', () => { brWin.webContents.openDevTools() }],  // 開発者ツールの起動
+                ['Ctrl+Shift+F1', () => {                                 // クライアントの再起動
                     app.relaunch();
                     app.quit();
                 }],
-            ];
-
-            sKeys.forEach((k) => {
+            ]; sKeys.forEach((k) => {
                 localShortcut.register(brWin, k[0], k[1]);
             });
         };
@@ -218,45 +155,31 @@ exports.socialWindow = class {
         initShortcutKeys();
         if (isSwapperEnabled) initSwapper(brWin);
         brWin.loadURL(url);
-        brWin.once('ready-to-show', () => {
-            brWin.show();
-        });
-        brWin.on('page-title-updated', (e) => {
-            e.preventDefault();
-        });
+        brWin.once('ready-to-show', () => { brWin.show() });
+        brWin.on('page-title-updated', (e) => { e.preventDefault() });
         brWin.webContents.on('will-prevent-unload', (e) => {
             if (!dialog.showMessageBoxSync({
                 buttons: [langPack.dialog.yes, langPack.dialog.no],
                 title: langPack.dialog.social.leavePageTitle,
                 message: langPack.dialog.social.leavePageMessage,
                 noLink: true,
-            })) {
-                e.preventDefault();
-            }
-        });
-        brWin.on('closed', () => {
+            })) { e.preventDefault() }
+        }); brWin.on('closed', () => {
             brWin.destroy();
             windows[lafTools.urlType(url)] = null;
-        });
-        brWin.webContents.on('new-window', (e, url) => {
+        }); brWin.webContents.on('new-window', (e, url) => {
             e.preventDefault();
             openNewWindow(url);
-        });
-        return brWin;
+        }); return brWin;
     }
 };
 
 const openNewWindow = (url) => {
-    if (lafTools.urlType(url) === 'external') {
-        shell.openExternal(url);
-        return;
-    }
     const winObj = windows[lafTools.urlType(url)];
+    
+    if (lafTools.urlType(url) === 'external') { shell.openExternal(url); return }
     if (winObj) {
         winObj.loadURL(url);
         winObj.on('closed', () => windows[lafTools.urlType(url)] = null);
-    }
-    else {
-        windows[lafTools.urlType(url)] = new this.socialWindow(url);
-    }
+    } else windows[lafTools.urlType(url)] = new this.socialWindow(url);
 };
