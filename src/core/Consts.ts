11489c39341fae28725fcca38acdd3cc9022a5e0
Hiro527 <hiro527.dev@gmail.com>
2022-04-22 22:06:40 +0900

    [CHANGE] ファイルの移動

--- src/core/Consts.ts
+++ src/core/Consts.ts
@@ -1,5 +1,5 @@
 // LaF Client 定数モジュール (c) 2022 Hiro527
-import { DataStore } from './@types/types';
+import { DataStore } from '../@types/types';
 
 export const Consts: DataStore = {
     DiscordClientID: '810350252023349248',
