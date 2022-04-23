f119b3ab1a3fd68b3466d3e4e9c60190e4853207
Hiro527 <hiro527.dev@gmail.com>
2022-04-24 03:59:13 +0900

    [ADD] UIのバックグラウンドを一部実装

---
+++ src/script/GameWindow.ts
@@ -0,0 +1,22 @@
+// LaF Client GameWindow Preload (c) 2022 Hiro527
+require('v8-compile-cache');
+import { app, BrowserWindow, clipboard, ipcRenderer } from 'electron';
+import Store from 'electron-store';
+import log from 'electron-log';
+import path from 'path';
+import isDev from 'electron-is-dev';
+
+import i18next from 'i18next';
+
+const PackageInfo = require('../../package.json');
+
+let i18n = i18next;
+const config = new Store();
+
+Object.assign(window, {
+    AppControl: (id: string) => {
+        switch (id) {
+            // idごとに処理(WIP)
+        }
+    },
+});
